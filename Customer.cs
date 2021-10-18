46ede533f54bb51f4a2ae0bcb71a97f46241faae
nilsner <79632260+nilsner@users.noreply.github.com>
2021-10-18 14:09:57 +0200

    Update Customer.cs

--- Customer.cs
+++ Customer.cs
@@ -1,20 +1,19 @@
-
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Projectwork.Data
+namespace OP2_Project.Data
 {
     public class Customer
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Phone{ get; set; }
-        public string Email{ get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
         public List<Order> order { get; set; }
 
-        public Customer() 
+        public Customer()
         {
             order = new List<Order>();
         }
