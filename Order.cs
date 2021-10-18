dd519f9b661e13538fc46e31d6b0877d95c20271
nilsner <79632260+nilsner@users.noreply.github.com>
2021-10-18 14:09:34 +0200

    Update Order.cs

--- Order.cs
+++ Order.cs
@@ -1,15 +1,14 @@
-
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Projectwork.Data
+namespace OP2_Project.Data
 {
     public class Order
     {
         public int Id { get; set; }
-        public int CustomerId{ get; set; }
+        public int CustomerId { get; set; }
         public Customer customer { get; set; }
         public DateTime OrderDate { get; set; }
         public string DeliveryAdress { get; set; }
@@ -19,6 +18,41 @@ namespace Projectwork.Data
         public Order()
         {
             items = new List<OrderLine>();
+        }
+
+        private ProdductContext prodductContext;
+        public List<OrderLine> orderLines;
+        public List<Product> products;
+        public List<Customer> customers;
+
+
+        public Order(ProdductContext context)
+        {
+            prodductContext = context;
+            orderLines = new List<OrderLine>();
+            products = new List<Product>();
+            customers = new List<Customer>();
+        }
+
+        
+        public void UpdateProduct(Product Products)
+        {
+            //görs lättast pages-filen. Se Nisses labb4
+        }
+
+        public async Task<List<Product>> AllProducts()
+        {
+            return await Task.Run(() => products);
+        }
+
+        public void AddProducts(Product Products)
+        {
+            products.Add(Products);
+        }
+
+        public void RemoveProducts(Product Products)
+        {
+            products.Remove(Products);
         }
     }
 }
