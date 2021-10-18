d11b5d1e7ebf4201fe1a370b8a6aed308267c78e
simon98sultan <65342868+simon98sultan@users.noreply.github.com>
2021-10-18 12:49:12 +0200

    Update Customer.cs

--- Customer.cs
+++ Customer.cs
@@ -1,1 +1,22 @@
 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Projectwork.Data
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Phone{ get; set; }
+        public string Email{ get; set; }
+        public List<Order> order { get; set; }
+
+        public Customer() 
+        {
+            order = new List<Order>();
+        }
+    }
+}
