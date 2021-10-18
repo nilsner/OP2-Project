2a170df79cc92ef674c367c23c980374b1773330
simon98sultan <65342868+simon98sultan@users.noreply.github.com>
2021-10-18 12:48:09 +0200

    Update Order.cs

--- Order.cs
+++ Order.cs
@@ -1,1 +1,24 @@
 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Projectwork.Data
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public int CustomerId{ get; set; }
+        public Customer customer { get; set; }
+        public DateTime OrderDate { get; set; }
+        public string DeliveryAdress { get; set; }
+        public bool PaymentCompleted { get; set; }
+        public bool Dispatched { get; set; }
+        public List<OrderLine> items { get; set; }
+        public Order()
+        {
+            items = new List<OrderLine>();
+        }
+    }
+}
