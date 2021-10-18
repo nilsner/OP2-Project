be7468711b4552ff5547600c72a21a92f3f46101
simon98sultan <65342868+simon98sultan@users.noreply.github.com>
2021-10-18 12:48:33 +0200

    Update OrderLine.cs

--- OrderLine.cs
+++ OrderLine.cs
@@ -1,1 +1,21 @@
 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Projectwork.Data
+{
+    public class OrderLine
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public Product product { get; set; }
+        public int OrderId { get; set; }
+        public int Quantity { get; set; }
+        public Order order { get; set; }
+        public OrderLine()
+        {
+        }
+    }
+}
