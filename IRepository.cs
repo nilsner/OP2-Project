276da89422d2342047c65188057c70e80dad70c5
nilsner <79632260+nilsner@users.noreply.github.com>
2021-10-19 17:29:51 +0200

    Add files via upload
    test for IRepository

---
+++ IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Op2_ver._2.Data
+{
+    public interface IRepository<T>
+    {
+        //ONLY CRUD HERE
+
+        Task<List<T>> ViewAllAsync();
+
+        Task<T> AddAsync(T prod);
+
+        Task<T> DeleteAsync(T prod);
+
+        void Update(T prod);
+    }
+}
